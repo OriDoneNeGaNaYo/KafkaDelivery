2218800c4cc6524575ccd137781c0491bb8f2d77
sky81219 <=>
2022-10-15 02:54:13 +0900

    kafka 튜닝 설계

---
+++ consumer.py
@@ -0,0 +1,8 @@
+from kafka import KafkaConsumer
+
+
+"""
+스트리밍 튜닝 
+"""
+class ConsumerCustomizer(KafkaConsumer):
+    pass
---
+++ producer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaProducer
+
+
+"""
+스트리밍 튜닝 
+"""
+class ProducerCuntomizer(KafkaProducer):
+    pass
+
+
